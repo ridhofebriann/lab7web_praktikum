99f0b8e0fe32cd81ccdcc10bc41407214a7eef2e
ridhofebriann <ridhogans69@gmail.com>
2026-06-11 01:30:53 +0700

    commit
    commit project

---
+++ lab8_vuejs/assets/js/components/About.js
@@ -0,0 +1,14 @@
+const About = {
+  template: `
+        <div class="home-container">
+            <h2>Profil Mahasiswa</h2>
+            <p><strong>Nama:</strong> M. Ridho Febrian</p>
+            <p><strong>NIM:</strong> 312410500</p>
+            <p><strong>Program Studi:</strong> Teknik Informatika</p>
+            <p><strong>Universitas:</strong> Universitas Pelita Bangsa</p>
+            <div style="margin-top: 15px;">
+                <img src="https://ui-avatars.com/api/?name=Ridho+Febrian&background=3152d6&color=fff&size=150" alt="Avatar Profil" style="border-radius: 50%;">
+            </div>
+        </div>
+    `,
+};
